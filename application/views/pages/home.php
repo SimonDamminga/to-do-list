f3a9a32b6250e349af09549b139550e6cf9416bc
Simon Damminga <99039973@mydavinci.nl>
2018-02-14 16:02:46 +0100

    lijst gefilterd

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -3,11 +3,14 @@
 <?php 
 
 $currList = null;
+$currentList = null;
 $myList = array();
+$tasks = array();
 
-class Task {
-    function Task($title){
+class ListItem {
+    function ListItem($title, $taken){
         $this->title = $title;
+        $this->taken = $taken;
     }
 }
 
@@ -15,21 +18,36 @@ class Task {
 
 ?>
 
-<?php foreach ($lists as $list):
+<?php 
 
-    if($currList != $list['listId']){
+foreach ($lists as $list):
+    if($currentList != $list['listId']){
 
-        $listItem = new Task($list['Title']);
+        foreach($lists as $listAsTask):
+            if($currentList != $listAsTask['listId']){
+                if($listAsTask['Description'] != ''){
+                    array_push($tasks, $listAsTask['Description']);
+                }else{
+                    $tasks = 'Geen taken';
+                }
+            }
+        endforeach;
+
+        $listItem = new ListItem($list['Title'], $tasks);
 
         array_push($myList, $listItem);
+        $currentList = $list['listId'];
+        $tasks = array();
     }
 
-    var_dump($myList);
+endforeach; 
+
+var_dump($myList);
 
 ?>
 
     
-
+<?php foreach ($lists as $list): ?>
 
     <?php if($currList != $list['listId']){ ?>
         <h3><?= $list['Title']; ?></h3>
